939340a3db1842ca529990edc58095997f8b14a6
felippedutra <felipe.id12@gmail.com>
2021-11-24 21:37:13 -0300

    Ajuste checkbox ativo/Inativo css

--- soulsoft_delivery_asp/Controllers/TiposProdutosController.cs
+++ soulsoft_delivery_asp/Controllers/TiposProdutosController.cs
@@ -1,16 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using soulsoft_delivery_asp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace soulsoft_delivery_asp.Controllers
 {
-    public class TiposMedidasController : Controller
+    public class TiposProdutosController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+        public TiposProdutosController()
         {
-            return View();
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.BaseAddress = new System.Uri("https://www.soulsoft.tec.br/api/");
+        }
+
+        [HttpGet]
+        public IActionResult Index(string TiposProdutosMessage = "")
+        {
+            if (TiposProdutosMessage != "")
+            {
+                ViewData["TiposProdutosMessage"] = TiposProdutosMessage;
+            }
+
+            HttpResponseMessage response = _httpClient.GetAsync("TipoProduto/Listar").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                dynamic responseJson = JsonConvert.DeserializeObject(responseString);
+                if (responseJson.status == "Sucesso")
+                {
+                    //Teste 1
+                    //dynamic TiposProdutos = JsonConvert.DeserializeObject<List<UsuarioApi>>(responseString.conteudo);
+
+                    //Teste 2
+                    JArray jObject = responseJson.conteudo as JArray;
+                    var TiposUsuarios = jObject.ToObject<List<TipoProdutoApiModel>>();
+
+                    return View(TiposUsuarios);
+                }
+            }
+            return View(new List<TipoProdutoApiModel>());
         }
 
         [HttpGet]
--- soulsoft_delivery_asp/Models/TipoProdutoApiModel.cs
+++ soulsoft_delivery_asp/Models/TipoProdutoApiModel.cs
@@ -9,13 +9,10 @@ namespace soulsoft_delivery_asp.Models
     public class TipoProdutoApiModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Informe o Nome")]
         [StringLength(150)]
-        [Required(ErrorMessage ="Informe o Nome")]
         public string Nome { get; set; }
         public DateTime Dt_cadastro { get; set; }
-        [StringLength(100)]
-        [Required(ErrorMessage = "Informe a Situação")]
-        public string Situacao { get; set; }
-
+        public bool Ativo { get; set; }
     }
 }
