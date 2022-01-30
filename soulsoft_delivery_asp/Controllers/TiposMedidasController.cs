a6cf463a355888dafba997d1f3eb9c4fdc3a101f
felippedutra <felipe.id12@gmail.com>
2022-01-30 20:47:00 -0300

    Ajuste add classe request Api Tipos de Medidas

--- soulsoft_delivery_asp/Controllers/TiposMedidasController.cs
+++ soulsoft_delivery_asp/Controllers/TiposMedidasController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using soulsoft_delivery_asp.Models;
+using soulsoft_delivery_asp.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,16 +16,10 @@ namespace soulsoft_delivery_asp.Controllers
 {
     public class TiposMedidasController : Controller
     {
-        private readonly HttpClient _httpClient;
-        public TiposMedidasController()
-        {
-            _httpClient = new HttpClient();
-            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            _httpClient.BaseAddress = new System.Uri("https://www.soulsoft.tec.br/api/");
-        }
+        const string LocalUrl = "/TipoMedida/";
 
         [HttpGet]
-        public IActionResult Index(string TiposMedidasMessage = "")
+        public async Task<IActionResult> Index(string TiposMedidasMessage = "")
         {
             if (TiposMedidasMessage != "")
             {
@@ -33,102 +28,67 @@ namespace soulsoft_delivery_asp.Controllers
 
             //Obtendo o EmpresaId
             int EmpresaId = (int)HttpContext.Session.GetInt32("_empresaId");
+            //Criando o objeto de lista
+            List<TipoMedidaApiModel> TiposMedidas = new List<TipoMedidaApiModel>();
 
-            HttpResponseMessage response = _httpClient.GetAsync($"TipoMedida/Listar/{EmpresaId}").Result;
-            if (response.IsSuccessStatusCode)
+            var TiposMedidasApi = await HttpClienteApi.NewGetAsync<List<TipoMedidaApiModel>>(LocalUrl + "Listar/" + EmpresaId);
+
+            if (TiposMedidasApi != null)
             {
-                var responseString = response.Content.ReadAsStringAsync().Result;
-                dynamic responseJson = JsonConvert.DeserializeObject(responseString);
-                if (responseJson.status == "Sucesso")
-                {
-                    //Teste 1
-                    //dynamic TiposProdutos = JsonConvert.DeserializeObject<List<UsuarioApi>>(responseString.conteudo);
-
-                    //Teste 2
-                    JArray jObject = responseJson.conteudo as JArray;
-                    var TiposMedidas = jObject.ToObject<List<TipoMedidaApiModel>>();
-
-                    return View(TiposMedidas);
-                }
+                TiposMedidas = TiposMedidasApi;
             }
-            return View(new List<TipoMedidaApiModel>());
+
+            return View(TiposMedidas);
         }
 
         [HttpGet]
-        public IActionResult CreateOrEdit(int TipoMedidaId = 0)
+        public async Task<IActionResult> CreateOrEdit(int Id = 0)
         {
-            if (TipoMedidaId != 0)
+            //Criando o objeto de retorno
+            TipoMedidaApiModel TipoMedida = new TipoMedidaApiModel();
+
+            if (Id != 0)
             {
-                HttpResponseMessage response = _httpClient.GetAsync($"TipoMedida/{TipoMedidaId}").Result;
-                if (response.IsSuccessStatusCode)
-                {
-                    var responseString = response.Content.ReadAsStringAsync().Result;
-                    dynamic responseJson = JsonConvert.DeserializeObject(responseString);
+                //Requisição de Tipo de Usuario
+                var TipoMedidaApi = await HttpClienteApi.NewGetAsync<List<TipoMedidaApiModel>>(LocalUrl + Id);
 
-                    JArray jObject = responseJson.conteudo as JArray;
-
-                    TipoMedidaApiModel TipoMedida = new TipoMedidaApiModel
-                    {
-                        Id = responseJson.conteudo[0].id,
-                        Nome = responseJson.conteudo[0].nome,
-                        Ativo = responseJson.conteudo[0].ativo
-                    };
-                    return View(TipoMedida);
-                }
-                else
+                if (TipoMedidaApi != null)
                 {
-                    //Retorna usuário não encontrado
-                    //return NotFound();
-                    return View(new TipoMedidaApiModel());
+                    TipoMedida.Id = TipoMedidaApi[0].Id;
+                    TipoMedida.EmpresaId = TipoMedidaApi[0].EmpresaId;
+                    TipoMedida.Nome = TipoMedidaApi[0].Nome;
+                    TipoMedida.Ativo = TipoMedidaApi[0].Ativo;
                 }
             }
-            else
-            {
-                return View(new TipoMedidaApiModel());
-            }
+
+            return View(TipoMedida);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult CreateOrEdit([Bind("Id", "EmpresaId", "Nome", "Ativo")] TipoMedidaApiModel TipoMedidaApi)
+        public async Task<IActionResult> CreateOrEdit([Bind("Id", "EmpresaId", "Nome", "Ativo")] TipoMedidaApiModel TipoMedidaApi)
         {
             if (ModelState.IsValid)
             {
-                //Vinculando o Usuário a empresa
-                TipoMedidaApi.EmpresaId = (int)HttpContext.Session.GetInt32("_empresaId");
-
                 if (TipoMedidaApi.Id == 0)
                 {
-                    TipoMedidaApi.DtCadastro = DateTime.Today;
+                    //Vinculando o Usuário a empresa
+                    TipoMedidaApi.EmpresaId = (int)HttpContext.Session.GetInt32("_empresaId");
+                    //Requisição para cadastro de Novo Tipo de Usuário
+                    var Response = await HttpClienteApi.NewPostAsync<object>(LocalUrl, TipoMedidaApi);
 
-                    string json = JsonConvert.SerializeObject(TipoMedidaApi);
-                    var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
-                    HttpResponseMessage response = _httpClient.PostAsync("TipoMedida", httpContent).Result;
-
-                    if (response.IsSuccessStatusCode)
+                    if (Response != null)
                     {
-                        string responseString = response.Content.ReadAsStringAsync().Result;
-                        dynamic responseJson = JsonConvert.DeserializeObject(responseString);
-                        if (responseJson.status == "Sucesso")
-                        {
-                            return Redirect("/TiposMedidas/Index?TiposMedidasMessage=Tipo de Medida cadastrado com Sucesso.");
-                        }
+                        return Redirect("/TiposMedidas/Index?TiposMedidasMessage=Tipo de Medida Cadastrado com Sucesso!");
                     }
                 }
                 else
                 {
-                    string json = JsonConvert.SerializeObject(TipoMedidaApi);
-                    var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
-                    HttpResponseMessage response = _httpClient.PutAsync("TipoMedida", httpContent).Result;
-
-                    if (response.IsSuccessStatusCode)
+                    //Requisição para Alteração de Tipo de Usuário
+                    var Response = await HttpClienteApi.NewPutAsync<object>(LocalUrl, TipoMedidaApi);
+                    if (Response != null)
                     {
-                        string responseString = response.Content.ReadAsStringAsync().Result;
-                        dynamic responseJson = JsonConvert.DeserializeObject(responseString);
-                        if (responseJson.status == "Sucesso")
-                        {
-                            return Redirect("/TiposMedidas/Index?TiposMedidasMessage=Tipo de Medida alterado com Sucesso.");
-                        }
+                        return Redirect("/TiposMedidas/Index?TiposMedidasMessage=Tipo de Medida Alterado com Sucesso!");
                     }
                 }
             }
@@ -136,16 +96,16 @@ namespace soulsoft_delivery_asp.Controllers
         }
 
         [HttpGet]
-        public IActionResult Delete(int TipoMedidaId)
+        public async Task<IActionResult> Delete(int Id = 0)
         {
-            HttpResponseMessage response = _httpClient.DeleteAsync($"TipoMedida/{TipoMedidaId}").Result;
-            if (response.IsSuccessStatusCode)
+            if (Id != 0)
             {
-                var responseString = response.Content.ReadAsStringAsync().Result;
-                dynamic responseJson = JsonConvert.DeserializeObject(responseString);
-                if (responseJson.status == "Sucesso")
+                //Requisição para Exclusão do Tipo de Usuário
+                var Response = await HttpClienteApi.NewDeleteAsync<object>(LocalUrl + Id);
+
+                if (Response != null)
                 {
-                    return Redirect("/TiposMedidas/Index?TiposMedidasMessage=Tipo de Medida removido com Sucesso.");
+                    return Redirect("/TiposMedidas/Index?TiposUsuariosMessage=Tipo de Medida Removido com Sucesso!");
                 }
             }
             return RedirectToAction("Index");
